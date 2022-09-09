efffec8fcc81e930670ce346bc01c12c6644472f
Rotem Tsubery <parotem@gmail.com>
2022-09-09 23:57:26 +0300

    first commit

---
+++ Final_Project_ASP_MVC/Repositories/AdminRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Final_Project_ASP_MVC.Data;
+using Final_Project_ASP_MVC.Models;
+
+namespace Final_Project_ASP_MVC.Repositories
+{
+    public class AdminRepository
+    {
+        private AnimalContext animalContext;
+        public AdminRepository(AnimalContext animalContext)
+        {
+            this.animalContext = animalContext;
+        }
+
+
+        public List<Animal> GetAllAnimals()
+        {
+            return animalContext.Animals!.ToList();
+        }
+
+
+    }
+}
+
